e71a6f203d165d9abf083f39564f50b2af3286fe
Abdur Rakib <abdurrakib9089@gmail.com>
2024-02-01 01:14:41 +0530

    frontend api updated1

--- src/main/java/com/zosh/config/AppConfig.java
+++ src/main/java/com/zosh/config/AppConfig.java
@@ -40,7 +40,7 @@ public class AppConfig {
 						
 						cfg.setAllowedOrigins(Arrays.asList(
 
-								"https://codewith-abdur.vercel.app"
+								"https://codewith-abdur-ow98eshx3-abdur-rakibs-projects.vercel.app/"
 								
 							)
 						);
